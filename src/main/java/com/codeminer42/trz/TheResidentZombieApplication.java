ae6af2aed2b49db96524969c93e1b7f3d7b106c4
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-07-28 14:15:12 -0300

    Initialize SpringBoot project

---
+++ src/main/java/com/codeminer42/trz/TheResidentZombieApplication.java
@@ -0,0 +1,13 @@
+package com.codeminer42.trz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheResidentZombieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheResidentZombieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeminer42/trz/TheResidentZombieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeminer42.trz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheResidentZombieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
